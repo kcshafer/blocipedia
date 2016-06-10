31f314d080742a01dfad34f0a16df472fb8a5b05
kcshafer <kcplusplus@live.com>
2016-06-10 15:47:29 -0400

    parsing markdown in wiki body

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
     def can_upgrade
         return current_user && current_user.role != 'premium'
     end
+
+    def to_markdown(t)
+        # renderer = Redcarpet::Render::HTML.new
+        # rc = Redcarpet::Markdown.new(renderer)
+        rc = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+        rc.render(t).html_safe
+    end
 end
